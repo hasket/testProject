bb90df2fc15cf36e97a2a195c55febf05ddb103d
Artem Vazerov <avazerov@inoo.tech>
2023-11-15 22:45:56 +0300

    сделал задачу

--- src/Practicum.java
+++ src/Practicum.java
@@ -1,20 +1,28 @@
+/* Задача
+Время применить полученные знания к Pair!
+ Ваши задачи:
+- Сделать переменные first и second приватными.
+- Создать конструктор класса Pair, присваивающий значения переменным класса.
+- Реализовать статический дженерик-метод of(), принимающий в качестве аргумента
+  два обобщённых параметра и возвращающий экземпляр Pair с этими параметрами.
+ */
+
 class Pair<F, S> {
-    public F first;
-    public S second;
+    private F first;
+    private S second;
+
+    Pair(F first, S second){
+        this.first = first;
+        this.second = second;
+    }
 
     @Override
     public String toString() {
         return "Pair{" + first + " " + second + "}";
     }
-}
 
-// Создайте дженерик-класс Triple, наследник класса Pair, содержащий одну переменную обобщенного типа с именем triple.
-
-class Triple<F,S,T> extends Pair<F,S> {
-    T triple;
-
-    @Override
-    public String toString() {
-        return "Triple{" + first + " " + second + " " + triple + "}";
+    public static <T, E> Pair of(T type, E element){
+        Pair<T,E> pairs = new Pair<>(type, element); // <- сделал экземпляр класса
+        return pairs;
     }
 }
